ddf83d44a9773967f9228ebf06dc8e4020b5fd13
SharePoint Development <spdevelopment@contoso.com>
2019-12-03 11:00:15 -0500

    Create dynamic component

---
+++ src/components/QuestionList.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import TagsList from './TagList';
+
+/**
+ * Each entry in the QuestionList is represtented by a QuestionListItem, which displays high-level information
+ * about a question in a format that works well in a list
+ */
+const QuestionListItem = ({title, tags})=>(
+    <div className="mb-3">
+        <h3>
+            {title}
+        </h3>
+ 
+        <div className="mb-2">
+            <TagsList tags={tags }/>  
+        </div>
+    </div>);
+
+/**
+ * Display all questions in an array provided to it as a simple list
+ */
+const QuestionList = ({questions})=>(
+    <div>
+        { questions ?
+            <div>
+                {questions.map(question=><QuestionListItem key={question.question_id} {...question}/>)}
+            </div> :
+            <div>
+                Loading questions...
+            </div>
+        }
+    </div>
+);
+
+/**
+ * Get the list of questions from the application's state
+ * It is populated by a ../sagas/fetch-question(s)-saga.
+ */
+const mapStateToProps = ({questions})=>({
+    questions
+});
+
+/**
+ * Create and export a connected component
+ */
+export default connect(mapStateToProps)(QuestionList);
