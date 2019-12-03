0f0056087d6479d0faec8f19257e2d57c4ed2f26
SharePoint Development <spdevelopment@contoso.com>
2019-12-03 09:05:04 -0500

    Addedreact@16.0.0 redux@3.7.2 react-redux@5.0.6 react-dom@16.0.0

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,12 @@
-console.info("react is cool");
+import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+const render = (_App) => {
+    ReactDOM.render(
+        <_App />,
+        document.getElementById('AppContainer')
+    )
+};
+
+render(App);
